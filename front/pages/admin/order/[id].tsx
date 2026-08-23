09b811c9e5ec3c3c2da23bba089795c95e118160
unknown <mr.krabopl12@gmail.com>
2026-08-23 20:06:43 +0400

    ssr redis

--- front/pages/admin/order/[id].tsx
+++ front/pages/admin/order/[id].tsx
@@ -1,3 +1,4 @@
 export { default } from 'src/pages/admin/orderDetails/OrderDetails';
-import { withMainData } from 'lib/withMainData';
-export const getStaticProps = withMainData();
+import { withMainDataServer } from 'lib/withMainData'; // 👈 Server версия
+
+export const getServerSideProps = withMainDataServer()
--- front/pages/admin/reset-password/[token].tsx
+++ front/pages/admin/reset-password/[token].tsx
@@ -1,3 +1,4 @@
 export { default } from 'src/pages/admin/resetPassword/ResetPassword';
-import { withMainData } from 'lib/withMainData';
-export const getStaticProps = withMainData();
+import { withMainDataServer } from 'lib/withMainData'; // 👈 Server версия
+
+export const getServerSideProps = withMainDataServer()
