56f4cab4485f3f4b64778e6b57dac6deebc48d4e
unknown <mr.krabopl12@gmail.com>
2026-08-25 12:08:28 +0400

    sharp

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -7,8 +7,8 @@ export const getStaticProps = async () => {
   console.log('[INDEX] ========================================');
   
   // 🔥 ЗАГРУЖАЕМ РЕАЛЬНЫЕ ДАННЫЕ
-  const [mainData, banners] = await Promise.all([
-    getMainData(),
+  const [ banners] = await Promise.all([
+    // getMainData(),
     getMainBanners().catch(() => [])
   ]);
   
@@ -22,33 +22,34 @@ export const getStaticProps = async () => {
   
   console.log('[INDEX] Тест данные:', testData);
   console.log('[INDEX] Реальные данные:');
-  console.log('[INDEX] - pageInfo length:', mainData?.pageInfo?.length || 0);
-  console.log('[INDEX] - categories length:', mainData?.mainInfo?.categories?.length || 0);
-  console.log('[INDEX] - firms length:', mainData?.mainInfo?.firms?.length || 0);
+  console.log('{BANNERS}',banners)
+  // console.log('[INDEX] - pageInfo length:', mainData?.pageInfo?.length || 0);
+  // console.log('[INDEX] - categories length:', mainData?.mainInfo?.categories?.length || 0);
+  // console.log('[INDEX] - firms length:', mainData?.mainInfo?.firms?.length || 0);
   
-  // 🔥 ПРОВЕРЯЕМ РАЗМЕР РЕАЛЬНЫХ ДАННЫХ
-  const realDataSize = JSON.stringify(mainData).length;
-  console.log('[INDEX] Реальные данные размер:', (realDataSize / 1024 / 1024).toFixed(2), 'MB');
+  // // 🔥 ПРОВЕРЯЕМ РАЗМЕР РЕАЛЬНЫХ ДАННЫХ
+  // const realDataSize = JSON.stringify(mainData).length;
+  // console.log('[INDEX] Реальные данные размер:', (realDataSize / 1024 / 1024).toFixed(2), 'MB');
   
   // 🔥 ПРОВЕРЯЕМ, ЧТО В pageInfo ЕСТЬ ПРОДУКТЫ
-  if (mainData?.pageInfo?.length > 0) {
-    const firstWidget = mainData.pageInfo[0];
-    console.log('[INDEX] Первый виджет:', firstWidget?.name);
-    console.log('[INDEX] Продуктов в первом виджете:', firstWidget?.products?.length || 0);
+  // if (mainData?.pageInfo?.length > 0) {
+  //   const firstWidget = mainData.pageInfo[0];
+  //   console.log('[INDEX] Первый виджет:', firstWidget?.name);
+  //   console.log('[INDEX] Продуктов в первом виджете:', firstWidget?.products?.length || 0);
     
-    // 🔥 ПРОВЕРЯЕМ ПЕРВЫЙ ПРОДУКТ НА ЦИКЛИЧЕСКИЕ ССЫЛКИ
-    if (firstWidget?.products?.length > 0) {
-      const firstProduct = firstWidget.products[0];
-      console.log('[INDEX] Первый продукт:', firstProduct?.name);
-      console.log('[INDEX] Ключи продукта:', Object.keys(firstProduct || {}));
-    }
-  }
+  //   // 🔥 ПРОВЕРЯЕМ ПЕРВЫЙ ПРОДУКТ НА ЦИКЛИЧЕСКИЕ ССЫЛКИ
+  //   if (firstWidget?.products?.length > 0) {
+  //     const firstProduct = firstWidget.products[0];
+  //     console.log('[INDEX] Первый продукт:', firstProduct?.name);
+  //     console.log('[INDEX] Ключи продукта:', Object.keys(firstProduct || {}));
+  //   }
+  // }
   
   return {
     props: {
       testData: testData, // 👈 ТЕСТОВЫЙ ОБЪЕКТ - ДОЛЖЕН БЫТЬ ВИДЕН В ЛОГАХ!
       initialData: {
-        ...mainData, // 👈 РЕАЛЬНЫЕ ДАННЫЕ
+        // ...mainData, // 👈 РЕАЛЬНЫЕ ДАННЫЕ
         banners: banners || []
       }
     },
